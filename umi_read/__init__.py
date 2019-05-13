31f10a41ec14d37000ceab4a5eb4348ee7f8a9a5
Matthew Watson <matt.sd.watson@gmail.com>
2019-05-13 16:46:18 -0400

    Update __init__.py

--- umi_read/__init__.py
+++ umi_read/__init__.py
@@ -1,3 +1,2 @@
-from umiread.umi_extract import extract_from_single, extract_from_folder
-from umiread.umi_stats import UMIStats
-
+from umi_read.umi_extract import extract_from_single, extract_from_folder
+from umi_read.umi_stats import UMIStats
