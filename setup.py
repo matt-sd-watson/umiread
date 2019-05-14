c830294d87b058dc83234cbab2c26e51edbf7b74
Matthew Watson <matt.sd.watson@gmail.com>
2019-05-14 09:32:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="umiread",
-    version="1.4.0",
+    version="1.6.0",
     author="Matthew Watson",
     author_email="matt.sd.watson@gmail.com",
     description="Lightweight utility for 10x single cell assays",
     long_description= long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/matt-sd-watson/umiread",
-    download_url="https://github.com/matt-sd-watson/umiread/archive/1.4.0.tar.gz",
+    download_url="https://github.com/matt-sd-watson/umiread/archive/1.6.0.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
